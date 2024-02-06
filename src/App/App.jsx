794ca8084f9a71d4c4dd55d61ad175d04d2af819
oleksii-zviezdin <oleksii.zviezdin@gmail.com>
2024-02-07 00:01:26 +0100

    added dependencies

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,22 +1,22 @@
 // src/App.js
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Homepage from "../pages/Homepage/Homepage";
-import Error from "../components/Error/Error";
-import { Layout } from "../components/Layout/Layout";
-import "./App.module.scss";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Homepage from '../pages/Homepage/Homepage';
+import Error from '../components/Error/Error';
+import { Layout } from '../components/Layout/Layout';
+import './App.module.scss';
 
 const App = () => {
-	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<Layout />}>
-					<Route index element={<Homepage />} />
-					<Route path="*" element={<Error />} />
-				</Route>
-			</Routes>
-		</Router>
-	);
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Homepage />} />
+          <Route path="*" element={<Error />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
